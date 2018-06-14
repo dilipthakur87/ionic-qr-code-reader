5977b056f0af53ddaf9b34d0020eada3645d0157
Dilip Thakur <dileepthakur87@gmail.com>
2018-06-14 19:24:51 +0545

    initialized ionic project with cordova phonegap-plugin-barcodescanner plugin

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  qrData = null;
+  createdCode = null;
+  scannedCode = null;
+ 
+  constructor(private barcodeScanner: BarcodeScanner) { }
+ 
+  createCode() {
+    this.createdCode = this.qrData;
   }
-
+ 
+  scanCode() {
+    this.barcodeScanner.scan(
+    	{ preferFrontCamera : true, // iOS and Android
+          showFlipCameraButton : true // iOS and Android
+        }).then(barcodeData => {
+      this.scannedCode = barcodeData.text;
+    }, (err) => {
+        console.log('Error: ', err);
+    });
+  }
 }
